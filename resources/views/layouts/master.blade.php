d2c1d2fe1335c9f9afee804abe42ca1ba7c2d8d6
PeggyLi <suara1201fxt@gmail.com>
2019-10-29 23:40:32 +0800

    feat: fix master blade css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" >
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/custom-app.css') }}" >
         @yield('header')
     </head>
     <body>
