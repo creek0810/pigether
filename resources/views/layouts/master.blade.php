5c878f93525a9550dd9d4f790a77a4831ca8d6a8
creek0810 <creek880810@gmail.com>
2019-12-15 22:40:15 +0800

    fix: update home url

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,7 +11,7 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-md navbar-dark bg-green">
-            <a class="navbar-brand" href="#">Pigether</a>
+            <a class="navbar-brand" href="/pigether">Pigether</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
